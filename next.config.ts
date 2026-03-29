1d2d1b0eae89e7e90704d0c82c3d73f6efb32094
Gaurav Kapur <gauravkapur596@gmail.com>
2026-03-30 04:13:07 +0530

    added logo and host ips

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  allowedDevOrigins: ["192.168.1.9"],
+  allowedDevOrigins: ["192.168.*", "10.*", "172.16.*"],
 };
 
 export default nextConfig;
